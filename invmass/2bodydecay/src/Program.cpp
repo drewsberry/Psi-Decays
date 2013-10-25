1f275831a4973cfd3580ab875bcf5ed5547df4c0
drewsberry <drewsberry@users.noreply.github.com>
2013-10-25 10:35:47 +0100

    Clean variable names

--- invmass/2bodydecay/src/Program.cpp
+++ invmass/2bodydecay/src/Program.cpp
@@ -9,14 +9,14 @@ Int_t Program::Code()
     if (!gROOT->GetClass("TGenPhaseSpace"))
         gSystem->Load("libPhysics"); // get and load TGenPhaseSpace class
  
-    TH1F *hist = new TH1F("#psi(3770) Meson Decay;m(D^{0}#bar{D^{0}}) [GeV/c^{2}];",
-            "Psi(3770) Decay", 50, 3, 4);
+    TH1F *psiInvMass = new TH1F("#psi(3770) Meson Decay;m(D^{0}#bar{D^{0}}) [GeV/c^{2}];",
+            "#psi(3770) Decay", 50, 3, 4);
     // Initialise histogram for plot later
  
-    TLorentzVector Psi_3770(0.0, 0.0, 0.0, 3.770);
+    TLorentzVector Psi_3770(0.0, 0.0, 0.0, PSIMASS);
     // Parent Psi 4-momentum in rest frame
  
-    Double_t masses[2] = { 1.86484, 1.86484 };
+    Double_t masses[2] = { DMASS, DMASS };
     // Product particle mass array
  
     TGenPhaseSpace event; // Declare event of type class TGenPhaseSpace
@@ -24,7 +24,7 @@ Int_t Program::Code()
  
     for (Int_t n = 0; n < 1000000; n++) // Loop 100,000 times
     {
-        Double_t        weight          = event.Generate();
+        Double_t        weight         = event.Generate();
         // Produce weighting for each event
  
         TLorentzVector  *D_0           = event.GetDecay(0);
@@ -34,20 +34,19 @@ Int_t Program::Code()
         TLorentzVector  D_0D_0bar      = *D_0 + *D_0bar;
         // Add 4-vectors together, for purpose of calculating invariant mass
  
-        hist -> Fill(D_0D_0bar.M(), weight);
+        psiInvMass -> Fill(D_0D_0bar.M(), weight);
         // Fill histogram with invariant mass
     }
  
-    TCanvas* canv = new TCanvas("canv", "Canvas", 500, 500);
+    TCanvas* PsiInvMassCanv = new TCanvas("PsiInvMassCanv", "PsiInvMassCanv", 500, 500);
  
-    hist->SetTitle("#psi(3770) Meson Decay;m(D^{0}#bar{D^{0}}) [GeV/c^{2}];");
-    hist->GetXaxis() -> CenterTitle();
-    hist->GetXaxis() -> SetTitleOffset(1.25);
-    hist->Draw();
+    psiInvMass->GetXaxis()->CenterTitle();
+    psiInvMass->GetXaxis()->SetTitleOffset(1.25);
+    psiInvMass->Draw();
  
-    canv->Print("../graphs/TwoBodyDecay.eps");
+    PsiInvMassCanv->Print("../graphs/TwoBodyDecay.eps");
 
-    delete canv;
+    delete PsiInvMassCanv;
     delete hist;
 
     return 0;
