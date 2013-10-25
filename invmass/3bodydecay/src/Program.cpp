7e4a5a5f644ce179a390dfa5484fb4954181ff7b
drewsberry <drewsberry@users.noreply.github.com>
2013-10-25 10:40:19 +0100

    Added better colour palette

--- invmass/3bodydecay/src/Program.cpp
+++ invmass/3bodydecay/src/Program.cpp
@@ -5,7 +5,16 @@ Int_t Program::Code()
 {
     /* Note that TFGenPhaseSpace, as per LHCb standard, is in GeV. Hence all the
      * calculations and graphs hereonin are in GeV. */
-
+     
+    const Int_t NRGBs = 5;
+    const Int_t NCont = 35;
+    Double_t stops[NRGBs] = { 0.00, 0.34, 0.61, 0.84, 1.00 };
+    Double_t red[NRGBs]   = { 0.00, 0.00, 0.87, 1.00, 0.51 };
+    Double_t green[NRGBs] = { 0.00, 0.81, 1.00, 0.20, 0.00 };
+    Double_t blue[NRGBs]  = { 0.51, 1.00, 0.12, 0.00, 0.00 };
+    TColor::CreateGradientColorTable(NRGBs, stops, red, green, blue, NCont);
+    gStyle -> SetNumberContours(NCont);
+    
     if (!gROOT->GetClass("TGenPhaseSpace")) // Get "TGenPhaseSpace" c;ass
         gSystem->Load("libPhysics"); // Load it if successful
 
